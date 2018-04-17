565cab4f96509ec5693dc77b4a336f0d0fe7fdc8
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-17 13:36:28 +0800

    Update blog/models Add choice field in blog/models

--- louis/webdev/myblog/blog/models.py
+++ louis/webdev/myblog/blog/models.py
@@ -7,7 +7,7 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 class Index(models.Model):
-    user                    = models.ForeignKey(User)
+    user                    = models.ForeignKey(User, on_delete=models.CASCADE)
     heading                 = models.CharField(max_length=120)
     sub_heading             = models.CharField(max_length=120)
 
@@ -15,32 +15,39 @@ class Index(models.Model):
         return '{}'.format(self.heading)
 
 class Tag(models.Model):
-    user                    = models.ForeignKey(User)
+    user                    = models.ForeignKey(User, on_delete=models.CASCADE)
     tag                     = models.CharField(max_length=120)
 
     def __str__(self):
         return '{}'.format(self.tag)
 
 class Category(models.Model):
-    user                    = models.ForeignKey(User)
+    user                    = models.ForeignKey(User, on_delete=models.CASCADE)
     category                = models.CharField(max_length=120)
 
     def __str__(self):
         return '{}'.format(self.category)
 
+
+POST_STATUS = (
+    ('published', 'Published'),
+    ('draft', 'Draft'),
+    ('hidden', 'Hidden')
+)
+
 class Post(models.Model):
-    user                    = models.ForeignKey(User)
-    post_list               = models.ForeignKey('Index', on_delete=models.CASCADE)
+    user                    = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_list               = models.ForeignKey('Index', on_delete=models.CASCADE,)
     title                   = models.CharField(max_length=120)
     subtitle_or_caption     = models.CharField(max_length=120)
     banner_photo            = models.ImageField()
     author                  = models.CharField(max_length=120)
     date_created            = models.DateTimeField(auto_now_add=True)
     date_modified           = models.DateTimeField(auto_now=True)
-    tags                    = models.ManyToManyField('Tag', on_delete=models.CASCADE)
-    category                = models.ForeignKey('Category', on_delete=models.CASCADE)
+    tags                    = models.ManyToManyField(Tag)
+    category                = models.ForeignKey('Category', on_delete=models.CASCADE,)
     body                    = models.TextField(max_length=1000)
-    status                  = models.CharField(max_length=120)
+    status                  = models.CharField(max_length=9, choices=POST_STATUS, default='published',)
 
     def __str__(self):
         return '{}'.format(self.post_list)
