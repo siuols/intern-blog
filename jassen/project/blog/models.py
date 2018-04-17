60572e6740eba498b4dcb2a098fef497e45564ad
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-17 11:30:37 +0800

    blog Model

--- jassen/project/blog/models.py
+++ jassen/project/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class blog(models.Model):
+    Heading = models.CharField(max_length=150)
+    Sub_Heading = models.CharField(max_length=150)
+
+
+
+class     
