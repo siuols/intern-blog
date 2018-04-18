1922ab2af54bca315dfc6a6b10856263556e3b7c
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-18 09:50:21 +0800

    redirect urls fix

--- jassen/project/project/urls.py
+++ jassen/project/project/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='grade')),
-    path('', RedirectView.as_view(url='/blog/', permanent=True)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
