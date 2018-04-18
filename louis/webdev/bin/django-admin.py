7c3f5ef2148e64ad451506d11fdaee453077f187
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-18 19:27:39 +0800

    Add pagination

---
+++ louis/webdev/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/louis/Documents/OJT/intern-blog/louis/webdev/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- louis/webdev/myblog/blog/views.py
+++ louis/webdev/myblog/blog/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from django.views import View
 
 from .models import Index, Post
 
 # Create your views here.
 
+# def listing(request):
+#     contact_list = Contact.objects.all()
+#     paginator = Paginator(contact_list, 25)
+
+#     page = request.GET.get('page')
+#     Contact = paginator.get_page(page)
+#     return render(request, 'list.html', {'contact': contacts})
+
+
 class IndexView(View):
-    def get(self, request, pk, *args, **kwargs):
-        index = Index.objects.filter(pk=pk).order_by('-date_created')
-        context = {
+  def get(self, request, pk, *args, **kwargs):
+    index = Index.objects.filter(pk=pk).order_by('-date_created')
+
+    post_list = Post.objects.all()
+
+    paginator = Paginator(post_list, 2)
+
+    page = request.GET.get('page')
+    post = paginator.get_page(page)
+
+    context = {
             'index': index,
-        }
-        return render(request, "blog/index.html", context)
+            'post': post,
+      }
+    return render(request, "blog/index.html", context)
 
 class PostView(View):
-    def get(self, request, post_id, *args, **kwargs):
-        post = Post.objects.filter(id=post_id).order_by('-date_created')
-        context = {
-            'object_list': post,
-        }
-        return render(request, "blog/post_list.html", context)
+  def get(self, request, post_id, *args, **kwargs):
+      post = Post.objects.filter(id=post_id).order_by('-date_created')
+      context = {
+          'object_list': post,
+      }
+      return render(request, "blog/post_list.html", context)
