beaa55c0d9d017029c2ee57f00155ca93928ef99
Cabilangan112 <jassencabilangan@gmail.com>
2018-04-18 09:45:58 +0800

    post indent fix

--- jassen/project/blog/models.py
+++ jassen/project/blog/models.py
@@ -35,13 +35,13 @@ class Post(models.Model):
         return '{}'.format(self.title)
 
 class Category(models.Model):
-    category_title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
 
     def __str__(self):
-        return '{}'.format(self.category_title)
+        return '{}'.format(self.title)
 
 class Tags(models.Model):
-    tags_title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
 
     def __str__(self):
-        return '{}'.format(self.tags_title)
+        return '{}'.format(self.title)
